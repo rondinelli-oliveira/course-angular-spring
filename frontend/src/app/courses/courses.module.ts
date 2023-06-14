22addb6947c2f19f8e472788e22258c53a1d719c
rondinelli-oliveira <rondinelliads@gmail.com>
2023-06-14 17:32:19 -0300

    error handling

--- frontend/src/app/courses/courses.module.ts
+++ frontend/src/app/courses/courses.module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from './../shared/shared.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
 
 @NgModule({
   declarations: [
@@ -14,6 +14,7 @@ import { CoursesComponent } from './courses/courses.component';
     AppMaterialModule,
     CommonModule,
     CoursesRoutingModule,
+    SharedModule
   ]
 })
 export class CoursesModule { }
