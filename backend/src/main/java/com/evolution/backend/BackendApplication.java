59949396f8f0285955ddc6878df76032a02949f8
rondinelli-oliveira <rondinelliads@gmail.com>
2023-06-14 23:21:07 -0300

    API http get

--- backend/src/main/java/com/evolution/backend/BackendApplication.java
+++ backend/src/main/java/com/evolution/backend/BackendApplication.java
@@ -1,13 +1,30 @@
 package com.evolution.backend;
 
+import com.evolution.backend.model.Course;
+import com.evolution.backend.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular");
+			c.setCategory("Front-end development");
+
+			courseRepository.save(c);
+		};
 	}
 
 }
---
+++ backend/src/main/java/com/evolution/backend/controller/CourseController.java
@@ -0,0 +1,24 @@
+package com.evolution.backend.controller;
+
+import com.evolution.backend.model.Course;
+import com.evolution.backend.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping()
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+}
