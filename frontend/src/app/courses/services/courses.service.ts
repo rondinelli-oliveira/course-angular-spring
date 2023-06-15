099777cf124c5a552858eb1d2771c23fb2da5ea9
rondinelli-oliveira <rondinelliads@gmail.com>
2023-06-15 12:38:35 -0300

    database H2 and connecting angular app to spring api

--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -9,7 +9,7 @@ import { delay, first, tap } from 'rxjs';
 })
 export class CoursesService {
 
-  private readonly URL = 'assets/courses.json';
+  private readonly URL = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
